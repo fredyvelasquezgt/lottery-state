860dfa7b422e542f1f422c45a8ddca3d79187145
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-10 13:23:41 -0600

    render two components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
     return (
       <div className='App'>
         <Lottery />
+        <Lottery title="Mini Daily" maxNum={10} maxBalls={4} />
+
       </div>
     );
   }
